d2142932addcda6166c86a9e2467a9830968b13d
Hien Nguyen <hien.nguyen@felizz.com>
2016-07-01 18:10:28 +0700

    Comment

--- app.js
+++ app.js
@@ -19,130 +19,104 @@ const FACEBOOK = {
     BACK_URL: '/auth/facebook/callback'
 }
 
-app.configure(function() {
-
-    /**
-     * Setup Mongo DB
-     */
-    mongoose.connect(MONGO.URL);
-    var UserSchema = new mongoose.Schema({
-        facebookId: {
-            type: String
-        },
-        access_token: {
-            type: String
-        },
-    });
-    UserSchema.statics.findOrCreate = function(filters, cb) {
-        User = this;
-        this.find(filters, function(err, results) {
-            if(results.length == 0) {
-                var newUser = new User();
-                newUser.facebookId = filters.facebookId;
-                newUser.save(function(err, doc) {
-                    cb(err, doc)
-                });
-            } else {
-                cb(err, results[0]);
-            }
-        });
-    };
-    var User = mongoose.model('User', UserSchema);
-
-    /**
-     *
-     * Authenticated by Facebook initially
-     */
-    options = {
-        clientID: FACEBOOK.APP_ID,
-        clientSecret: FACEBOOK.APP_SECRET,
-        callbackURL: FACEBOOK.BACK_URL
-    };
-
-    passport.use(
-        new FacebookStrategy(
-            options,
-            function(accessToken, refreshToken, profile, done) {
-                User.findOrCreate(
-                    {
-                        facebookId: profile.id
-                    },
-                    function (err, result) {
-
-                        if(result) {
-                            console.log("Found/Created user: " + JSON.stringify(result));
-                            result.access_token = accessToken;
-                            result.save(function(err, doc) {
-                                if(err){
-                                    console.log("Failed to save user: ", err);
-                                } else {
-                                    console.log("Saved user: ", doc);
-                                }
-
-                                done(err, doc);
-                            });
-                        } else {
-                            console.log("Failed to retrieve/create user. Error: ", err);
-                            done(err, result);
-                        }
-                    }
-                );
-            }
-        )
-    );
-
-    app.get(
-        FACEBOOK.OUT_URL,
-        passport.authenticate(FACEBOOK.NAME,
-            {
-                session: false,
-                scope: []
-            }
-        )
-    );
-
-    app.get(FACEBOOK.BACK_URL,
-        passport.authenticate(FACEBOOK.NAME,
-            {
-                session: false,
-                failureRedirect: "/"
-            }
-        ),
-        function(req, res) {
-            res.redirect(`/profile?access_token=${req.user.access_token}`);
-        }
-    );
-
-    /**
-     * Using a token in subsequent request
-     */
-    passport.use(
-        new BearerStrategy(
-            function(token, done) {
-                console.log("Received token: " + token);
-                User.findOne(
-                    {
-                        access_token: token
-                    },
-                    function(err, user) {
-                        if(err) {
-                            console.log("Failed (system) to retrieve user from token: ", token);
-                            return done(err);
-                        }
-                        if(!user) {
-                            console.log("Failed (not found) to retrieve user from token: ", token);
-                            return done(null, false);
-                        }
-
-                        console.log("Retrieved user: ", user , " from token: ", token);
-                        return done(null, user, { scope: 'all' })
-                    }
-                );
-            }
-        )
-    );
+/**
+ * Setup Mongo DB
+ */
+mongoose.connect(MONGO.URL);
+var UserSchema = new mongoose.Schema({
+    facebookId: {
+        type: String
+    },
+    access_token: {
+        type: String
+    },
 });
+UserSchema.statics.findOrCreate = function(filters, cb) {
+    User = this;
+    this.find(filters, function(err, results) {
+        if(results.length == 0) {
+            var newUser = new User();
+            newUser.facebookId = filters.facebookId;
+            newUser.save(function(err, doc) {
+                cb(err, doc)
+            });
+        } else {
+            cb(err, results[0]);
+        }
+    });
+};
+var User = mongoose.model('User', UserSchema);
 
+/**
+ *
+ * Authenticated by Facebook initially
+ */
+options = {
+    clientID: FACEBOOK.APP_ID,
+    clientSecret: FACEBOOK.APP_SECRET,
+    callbackURL: FACEBOOK.BACK_URL
+};
+
+passport.use(
+    new FacebookStrategy(
+        options,
+        function(accessToken, refreshToken, profile, done) {
+            User.findOrCreate(
+                {
+                    facebookId: profile.id
+                },
+                function (err, result) {
+
+                    if(result) {
+                        console.log("Found/Created user: " + JSON.stringify(result));
+                        result.access_token = accessToken;
+                        result.save(function(err, doc) {
+                            if(err){
+                                console.log("Failed to save user: ", err);
+                            } else {
+                                console.log("Saved user: ", doc);
+                            }
+
+                            done(err, doc);
+                        });
+                    } else {
+                        console.log("Failed to retrieve/create user. Error: ", err);
+                        done(err, result);
+                    }
+                }
+            );
+        }
+    )
+);
+
+/**
+ * Using a token in subsequent request
+ */
+passport.use(
+    new BearerStrategy(
+        function(token, done) {
+            console.log("Received token: " + token);
+            User.findOne(
+                {
+                    access_token: token
+                },
+                function(err, user) {
+                    if(err) {
+                        console.log("Failed (system) to retrieve user from token: ", token);
+                        return done(err);
+                    }
+                    if(!user) {
+                        console.log("Failed (not found) to retrieve user from token: ", token);
+                        return done(null, false);
+                    }
+
+                    console.log("Retrieved user: ", user , " from token: ", token);
+                    return done(null, user, { scope: 'all' })
+                }
+            );
+        }
+    )
+);
 
 /**
  * HTML routing setup
@@ -153,10 +127,14 @@ app.get('/',
     }
 );
 
+// Use something like Redis and set a ttl on the token, so the token is removed automatically after some time.
 app.get('/logout',
         function(req, res){
+        req.logout();   //Invoking logout() will remove the req.user property and clear the login session
 
-        req.logout();
+        var access_token = req.query.access_token;
+        console.log("Log out acess token: ", access_token);
+
         res.redirect('/');
     }
 );
@@ -165,8 +143,30 @@ app.get('/profile',
     passport.authenticate('bearer', { session: false }),
     function(req, res) {
         res.send(
-            `LOGGED IN as ${req.user.facebookId} - <a href="/logout">Log out</a>`
+            `LOGGED IN as ${req.user.facebookId} - <a href="/logout?access_token=${req.user.access_token}">Log out</a>`
         );
+    }
+);
+
+app.get(
+    FACEBOOK.OUT_URL,
+    passport.authenticate(FACEBOOK.NAME,
+        {
+            session: false,
+            scope: []
+        }
+    )
+);
+
+app.get(FACEBOOK.BACK_URL,
+    passport.authenticate(FACEBOOK.NAME,
+        {
+            session: false,
+            failureRedirect: "/"
+        }
+    ),
+    function(req, res) {
+        res.redirect(`/profile?access_token=${req.user.access_token}`);
     }
 );
 
